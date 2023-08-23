449c68c95f45bf9cad9d3b6f318de20461604cd4
Nami <havenothorn@gmail.com>
2023-08-23 16:04:51 +0900

    style: 날씨 API 한국어 버전 적용

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "9f673e84985d181d2f1c2b905365e177";
 function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
