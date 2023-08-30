f9141202f79c60046ccd1424f398f7b4829e1a1a
Nami <havenothorn@gmail.com>
2023-08-30 23:19:37 +0900

    refactor: 날씨 영역 수정 - Js 구조 분할 - html 내부 변경

--- js/weather.js
+++ js/weather.js
@@ -1,24 +1,35 @@
 const API_KEY = "9f673e84985d181d2f1c2b905365e177";
 
+const weather = document.querySelector("#weather");
+
+function displayWeather(data) {
+  const { name } = data;
+  const { description, icon } = data.weather[0];
+  const { temp } = data.main;
+
+  const weatherImg = document.createElement("img");
+  weatherImg.src = `../img/icons/${icon}.png`;
+
+  const weatherTxt = document.createElement("span");
+  weatherTxt.innerText = `${description} · ${Math.round(temp)}°C `;
+
+  const city = document.createElement("span");
+  city.innerText = `· 📍 ${name}`;
+
+  weather.appendChild(weatherImg);
+  weather.appendChild(weatherTxt);
+  weather.appendChild(city);
+}
+
 function onGeoOK(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
+  const { latitude: lat, longitude: lon } = position.coords;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
   fetch(url)
     .then((response) => response.json())
-    .then((data) => {
-      const weather = document.querySelector("#weather");
-      const weatherImg = document.createElement("img");
-      weatherImg.src = `../img/icons/${data.weather[0].icon}.png`;
-      weather.prepend(weatherImg);
-      const weatherTxt = weatherImg.nextElementSibling;
-      weatherTxt.innerText = `${data.weather[0].description} · ${Math.round(
-        data.main.temp
-      )}°C `;
-      const city = weather.querySelector("span:last-child");
-      city.innerText = `· 📍 ${data.name}`;
-    });
+    .then(displayWeather);
 }
+
 function onGeoError() {
   alert("Can't find you");
 }
