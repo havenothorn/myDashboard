d2962dd7153513ba7c56efc78ded4ffc9947c88e
Nami <havenothorn@gmail.com>
2023-09-08 01:41:04 +0900

    style: CSS BEM 방법론 적용 - quote 영역

--- js/quotes.js
+++ js/quotes.js
@@ -49,8 +49,8 @@ const quotes = [
   },
 ];
 
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+const quote = document.querySelector(".quote span:first-child");
+const author = document.querySelector(".quote span:last-child");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
